dc1bed4cae862ad6552726b3bf78651bff894a2f
Edwin Rugoogamu <rugoogamuedwin5@gmail.com>
2023-07-03 23:51:35 +0300

    updating the model

--- models/registerModel.js
+++ models/registerModel.js
@@ -1,42 +1,15 @@
 const mongoose = require("mongoose");
 
 const registerSchema = new mongoose.Schema({
-    surname:{
+    secondName:{
         type:String,
         trim:true
     },
-    givenname: {
+    firstName: {
         type:String,
         trim:true
     },
-    title:{
-        type:String,
-        trim:true
-    },
-    photo:{
-        type:String,
-        trim:true
-    },
-    gender:{
-        type:String,
-        trim:true
-    },
-    dob:{
-        type:String,
-        trim:true
-    },
-    country:{
-        type:String,
-        trim:true
-    },
-    residence:{
-        type:String,
-        trim:true
-    },
-    nationality:{
-        type:String,
-        trim:true
-    }
+   
 
 })
 
--- routes/calculatorRoutes.js
+++ routes/calculatorRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const Register = require('../models/registerModel')
-const multer = require("multer")
+
 
 
  
